d3cc28dfe4e81c4640b7503febc7a7e58d77fc68
iampatil <avinashpatil880@outlook.com>
2021-10-21 22:49:05 +0530

    First commit

--- springboot-cassandra-demo/src/main/java/com/javatutor/cassandra/repo/ProductRepo.java
+++
@@ -1,9 +0,0 @@
-package com.javatutor.cassandra.repo;
-
-import org.springframework.data.cassandra.repository.CassandraRepository;
-
-import com.javatutor.cassandra.model.Product;
-
-public interface ProductRepo extends CassandraRepository<Product, Integer> {
-
-}
